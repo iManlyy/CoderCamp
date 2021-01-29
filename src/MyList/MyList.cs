ebc64161795f615c3d67d0e0904e4ec1b3d4c61b
Manly-y <lrmertylmmz@gmail.com>
2021-01-29 20:45:02 +0300

    Proje Sonu

---
+++ src/MyList/MyList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class MyList<T>
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            T [] tempArray = new T[0];
+            tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        //public int Length
+        //{
+        //    get { return _array.Length; }
+        //}
+    }
+}
--- src/MyList/Program.cs
+++ src/MyList/Program.cs
@@ -9,8 +9,17 @@ namespace MyList
     {
         public static void Main(string[] args)
         {
-            List<string> hazirListe = new List<string> { "fghfg", "skjsj", "cvcv", "qwe", "asd" };
-            Console.WriteLine(hazirListe[2]);
+            //List<string> hazirListe = new List<string> { "fghfg", "skjsj", "cvcv", "qwe", "asd" };
+            //Console.WriteLine(hazirListe[2]);
+
+            MyList<int> myList = new MyList<int>();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(40);
+            myList.Add(70);
+            //Console.WriteLine(myList.Length);
+            myList.Add(20);
+            //Console.WriteLine(myList.Length);
             Console.ReadKey();
         }
     }
